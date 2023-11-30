73195ad00cd6666644e296dab3d8e83ce92aeab8
mahesh <it.mahesh11@gmail.com>
2023-11-30 14:42:08 +0530

    update latest

--- test/test/src/main/java/com/git/test/Main.java
+++ test/test/src/main/java/com/git/test/Main.java
@@ -1,7 +1,24 @@
-package com.git.test;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello worlsdd!");
+
+    public String test(String fileName, String message)
+    {
+        System.out.println("Hello world!");
+
+        // File write program
+        String filePath = fileName;
+        String content = message;
+
+        try (FileWriter writer = new FileWriter(filePath)) {
+            writer.write(content);
+            System.out.println("File written successfully.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing the file: " + e.getMessage());
+        }
+
+        return message;
+
     }
 }
